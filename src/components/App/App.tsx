e3dde36bab700422d1d80a7c5c6748744250008c
devolexa <starovatovasasha@gmail.com>
2026-01-27 18:00:34 +0200

    Create interface for a note

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,15 @@
-import './App.module.css'
+import css from './App.module.css'
 
 export default function App() {
-    return ()
+    return (
+        <div className={css.app}>
+            <header className={css.toolbar}>
+                {/* Компонент SearchBox */}
+                {/* Пагінація */}
+                {/* Кнопка створення нотатки */}
+            </header>
+        </div>
+
+    )
 }
 
