3a324dd4b0248d68d1dffc612ff13b3adc123eff
devolexa <starovatovasasha@gmail.com>
2026-02-01 11:55:01 +0200

    Update Modal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,8 @@
 import css from './App.module.css'
 import NoteList from '../NoteList/NoteList'
 import SearchBox from '../SearchBox/SearchBox'
+import NoteForm from '../NoteForm/NoteForm'
+import Modal from '../Modal/Modal'
 import { useQuery } from '@tanstack/react-query'
 import { fetchNotes } from '../../services/noteService'
 import { useState } from 'react'
@@ -10,8 +12,12 @@ import { useDebouncedCallback } from 'use-debounce'
 export default function App() {
     const [searchQuery, setSearchQuery] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
-    const debouncedSetSearchQuery = useDebouncedCallback(setSearchQuery, 300)
+    const debouncedSetSearchQuery = useDebouncedCallback(setSearchQuery, 300);
+    const [openModal, setOpenModal] = useState(false)
 
+    const handleOpenModal = () => {
+        setOpenModal(true)
+    }
 
     const { data, isLoading } = useQuery({
         queryKey: ['notes', searchQuery, currentPage],
@@ -24,12 +30,17 @@ export default function App() {
 
                 <SearchBox text={searchQuery} onSearch={debouncedSetSearchQuery} />
                 {/* Пагінація */}
-                {/* Кнопка створення нотатки */}
+                <button className={css.button} onClick={handleOpenModal}>Create note +</button>
             </header>
             {data && data.length > 0 ? (
                 <NoteList notes={data} />
             ) : (
                 !isLoading && <p>Нотаток поки немає. Створіть першу!</p>
+            )}
+            {openModal && (
+                <Modal onClose={() => setOpenModal(false)}>
+                    <NoteForm onClose={() => setOpenModal(false)} />
+                </Modal>
             )}
         </div>
 
