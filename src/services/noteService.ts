fb3c34018cc0857a121fc9db122361eaa8adc185
devolexa <starovatovasasha@gmail.com>
2026-02-01 14:05:55 +0200

    Fix crud

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,14 +1,17 @@
 import type { Note } from "../types/note";
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
+axios.defaults.baseURL = "https://notehub-public.goit.study/api";
 
 interface FetchNotesResponse {
-    notes: Note[],
+    notes: Note[];
     totalPages: number;
 }
 
-export async function fetchNotes(query: string, page: number): Promise<FetchNotesResponse> {
+export async function fetchNotes(
+    query: string,
+    page: number,
+): Promise<FetchNotesResponse> {
     try {
         const { data } = await axios.get<FetchNotesResponse>("/notes", {
             params: {
@@ -16,14 +19,14 @@ export async function fetchNotes(query: string, page: number): Promise<FetchNote
                 page: page,
             },
             headers: {
-                Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`
-            }
-        })
-        console.log(data)
+                Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+            },
+        });
+        console.log(data);
         return {
             notes: data.notes || [],
-            totalPages: data.totalPages || 1
-        }
+            totalPages: data.totalPages || 1,
+        };
     } catch (error) {
         console.error("Помилка при завантаженні нотаток:", error);
         throw error;
@@ -32,29 +35,30 @@ export async function fetchNotes(query: string, page: number): Promise<FetchNote
 
 export type CreateNoteInForm = Omit<Note, "id" | "createdAt" | "updatedAt">;
 
-export async function createNote(noteData: CreateNoteInForm) {
-    const { data } = await axios.post<Note>("/notes", noteData, {
-        headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`
-        }
-    });
-    return data;
-}
-
-interface DeleteNoteResponse {
-    note: Note;
-}
-
-export async function deleteNote(id: string) {
+export async function createNote(noteData: CreateNoteInForm): Promise<Note> {
     try {
-        const { data } = await axios.delete<DeleteNoteResponse>(`/notes/${id}`, {
+        const { data } = await axios.post<Note>("/notes", noteData, {
             headers: {
-                Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`
-            }
+                Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+            },
         });
-        return data
+        return data;
     } catch (error) {
-        console.log(error)
+        console.error("Error creating note:", error);
+        throw error;
     }
+}
 
+export async function deleteNote(id: string): Promise<Note> {
+    try {
+        const { data } = await axios.delete<Note>(`/notes/${id}`, {
+            headers: {
+                Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+            },
+        });
+        return data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
