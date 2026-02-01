da8487aad5d440a077ce39beef640b8607091344
devolexa <starovatovasasha@gmail.com>
2026-02-01 14:20:06 +0200

    Delete one onSuccess()

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -39,11 +39,6 @@ export default function NoteForm({ onClose }: NoteFormProps) {
 
     const { mutate: createMutate } = useMutation({
         mutationFn: createNote,
-
-        onSuccess() {
-            queryClient.invalidateQueries({ queryKey: ["notes"] });
-            onClose();
-        },
     });
     const handleSubmit = (
         values: CreateNoteInForm,
@@ -51,6 +46,7 @@ export default function NoteForm({ onClose }: NoteFormProps) {
     ) => {
         createMutate(values, {
             onSuccess: () => {
+                queryClient.invalidateQueries({ queryKey: ['notes'] })
                 actions.resetForm();
                 onClose()
             },
